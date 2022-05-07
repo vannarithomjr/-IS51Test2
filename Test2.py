61e31f5b58e694d986e0bd8acacefc775362ef36
Vannarith Om Jr <vannarithomjr@vannarithsmbp2.lan>
2022-05-06 23:40:42 -0700

    pseudo-code done

--- Test2.py
+++ Test2.py
@@ -3,3 +3,32 @@ This program opens a file Final.txt with the grades of students, stores the grad
 to an empty list of integers, displays the number of grades,
 the average grade, and the percent of grades that are above the average grade.
 """
+
+
+"""
+main
+    calculate_percent_above_average
+
+calculate_percent_above_average:
+    grades
+    open file
+    for loop
+        append each number to grades list
+    close file
+
+    number of grades = length of grades
+    average grade = sum of all the grades / number of grades
+    
+    set above average counter to 0
+    for loop in grades:
+        if grades[index] > average grade:
+            above average counter + above average counter + 1
+        
+    above average percentage = (above average counter / number of grades) * 100
+
+    print number of grades (24)
+    print average grade (83.25)
+    print percentage of grades before average (54.17)
+
+call main
+"""
