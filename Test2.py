055d61435a225402817d12a5cecc36ebabc88595
Vannarith Om Jr <vannarithomjr@vannarithsmbp2.lan>
2022-05-06 22:41:02 -0700

    structured english done

---
+++ Test2.py
@@ -0,0 +1,5 @@
+"""
+This program opens a file Final.txt with the grades of students, stores the grades
+to an empty list of integers, displays the number of grades,
+the average grade, and the percent of grades that are above the average grade.
+"""
